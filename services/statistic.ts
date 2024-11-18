646c0bb282a61a84de352256f5c7ecc61c403c1e
lasbe <devlasbe@gmail.com>
2024-11-18 17:45:39 +0900

    feat: statistic api service 추가

--- services/statistic.ts
+++ services/statistic.ts
@@ -1,10 +1,21 @@
 import myFetch from "@/lib/myFetch";
-import { GetStartupRes, GetStatisticListRes } from "@/types/apiTypes";
+import { GetStartupRes, GetStatisticByFilterReq, GetStatisticListRes } from "@/types/apiTypes";
 
 export class StatisticService {
   static async getStatistic(name: string) {
     const dataList = await myFetch<GetStatisticListRes>(`statistic/${name}`);
     return dataList;
+  }
+  static async getStatisticByFilter(params: GetStatisticByFilterReq) {
+    const stringParams = Object.entries(params).reduce(
+      (acc, [key, value]) => {
+        acc[key] = String(value);
+        return acc;
+      },
+      {} as Record<string, string>
+    );
+    const data = await myFetch<GetStatisticListRes>("statistic?" + new URLSearchParams(stringParams));
+    return data;
   }
   static async getStartup(name: string) {
     const dataList = await myFetch<GetStartupRes>(`startups/${name}`);
