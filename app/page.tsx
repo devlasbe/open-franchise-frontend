1da036065f4da6b2193434b0535a2290452c423b
lasbe <devlasbe@gmail.com>
2024-11-18 17:44:31 +0900

    refactor: Rank 컴포넌트 분리

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,34 @@
-import Image from "next/image";
+import SearchInput from "@/components/SearchInput";
+import Rank from "./_main/Rank";
+import FetchBoundary from "@/components/errorBoundary/FetchBoundary";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col gap-8 w-full">
+      <div className="flex flex-col flex-1 gap-4 sm:gap-8 justify-center items-center p-8 sm:p-12 max-h-[200px] sm:max-h-[300px] bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg">
+        <div className="space-y-2 text-center text-white">
+          <p className="font-[900] sm:font-[900] text-h3 sm:text-h1">궁금한 프렌차이즈 정보를 한눈에</p>
+          <p className="text-subtitle3 sm:text-h3">가맹정보부터 매출액까지 무료로 확인하세요</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="sm:max-w-[600px] w-full bg-white shadow-md rounded-full">
+          <SearchInput />
+        </div>
+      </div>
+      <FetchBoundary>
+        <Rank category="치킨" title="🍗 치킨 TOP10" />
+      </FetchBoundary>
+      <FetchBoundary>
+        <Rank category="편의점" title="🏪 편의점 TOP10" />
+      </FetchBoundary>
+      <FetchBoundary>
+        <Rank category="커피" title="☕️ 카페 TOP10" />
+      </FetchBoundary>
+      <FetchBoundary>
+        <Rank category="주점" title="🍺 주점 TOP10" />
+      </FetchBoundary>
+      <FetchBoundary>
+        <Rank category="피자" title="🍕 피자 TOP10" />
+      </FetchBoundary>
     </div>
   );
 }
