6e1832bb232c8d4100d2041fdb2d2d2bd8808ba6
lasbe <devlasbe@gmail.com>
2024-11-20 08:21:54 +0900

    chore: deploy test

--- lib/myFetch.ts
+++ lib/myFetch.ts
@@ -1,18 +1,22 @@
-const apiDev = process.env.API_URL_DEV;
-const apiOp = process.env.API_URL_OP;
+const apiDev = process.env.NEXT_PUBLIC_API_URL_DEV;
+const apiOp = process.env.NEXT_PUBLIC_API_URL_OP;
 const isDev = process.env.NODE_ENV === "development";
 const defaultUrl = isDev ? apiDev : apiOp;
 
 const myFetch = async <T>(input: RequestInfo | URL, init?: RequestInit) => {
-  const endPoint = `${defaultUrl}/${input}`;
-  const response = await fetch(endPoint, init);
-  const data: T = await response.json();
-  if (!response.ok) {
-    const error = data as { response: { message: string } };
-    console.error(endPoint, error);
-    throw new Error(error?.response?.message);
+  try {
+    const endPoint = `${defaultUrl}/${input}`;
+    const response = await fetch(endPoint, init);
+    const data: T = await response.json();
+    if (!response.ok) {
+      const error = data as { response: { message: string } };
+      console.error(endPoint, error);
+      throw new Error(error?.response?.message);
+    }
+    return data;
+  } catch (error) {
+    throw error;
   }
-  return data;
 };
 
 export default myFetch;
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const apiDev = process.env.API_URL_DEV;
-const apiOp = process.env.API_URL_OP;
+const apiDev = process.env.NEXT_PUBLIC_API_URL_DEV;
+const apiOp = process.env.NEXT_PUBLIC_API_URL_OP;
 const isDev = process.env.NODE_ENV === "development";
 const defaultUrl = isDev ? apiDev : apiOp;
 
