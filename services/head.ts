13ff59607e0c6419e10c722bb539a220bc692c29
lasbe <devlasbe@gmail.com>
2024-11-20 08:29:13 +0900

    chore: deploy test

--- services/head.ts
+++ services/head.ts
@@ -4,8 +4,8 @@ import { QueryParamsUtil } from "@/utils/queryParams";
 
 export class HeadService {
   static async getHead(jnghdqrtrsMnno: string) {
-    const year = process.env.DEFAULT_YEAR;
-    if (!year) throw new Error("DEFAULT_YEAR does not exist");
+    const year = process.env.NEXT_PUBLIC_DEFAULT_YEAR;
+    if (!year) throw new Error("NEXT_PUBLIC_DEFAULT_YEAR does not exist");
     const params: GetHeadReq = {
       jnghdqrtrsMnno,
       jngBizCrtraYr: year,
