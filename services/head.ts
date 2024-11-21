a7f7bdb55838286904bc084e6da9017e9d2e6a5f
lasbe <devlasbe@gmail.com>
2024-11-21 12:11:07 +0900

    chore: head service 불필요한 쿼리 파라미터 제거

--- services/head.ts
+++ services/head.ts
@@ -1,14 +1,11 @@
 import myFetch from "@/lib/myFetch";
-import { GetHeadReq, GetHeadRes } from "@/types/apiTypes";
+import { GetHeadRes } from "@/types/apiTypes";
 import { QueryParamsUtil } from "@/utils/queryParams";
 
 export class HeadService {
   static async getHead(jnghdqrtrsMnno: string) {
-    const year = process.env.NEXT_PUBLIC_DEFAULT_YEAR;
-    if (!year) throw new Error("NEXT_PUBLIC_DEFAULT_YEAR does not exist");
-    const params: GetHeadReq = {
+    const params = {
       jnghdqrtrsMnno,
-      jngBizCrtraYr: year,
     };
     const dataList = await myFetch<GetHeadRes>({ path: QueryParamsUtil.convert("heads", params) });
     return dataList;
