689186222b7763c4e68d0d7a19659b491ed6044a
lasbe <devlasbe@gmail.com>
2025-01-28 16:59:46 +0900

    chore: console log 제거

--- lib/myFetch.ts
+++ lib/myFetch.ts
@@ -1,6 +1,6 @@
 const apiDev = process.env.NEXT_PUBLIC_API_URL_DEV;
 const apiOp = process.env.NEXT_PUBLIC_API_URL_OP;
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === 'development';
 const defaultUrl = isDev ? apiDev : apiOp;
 
 type MyFetchType = {
@@ -12,12 +12,11 @@ type MyFetchType = {
 const myFetch = async <T>({ path, init, isClient }: MyFetchType) => {
   try {
     const endPoint = isClient ? `/franchise/${path}` : `${defaultUrl}/${path}`;
-    console.log("[Request]", endPoint);
     const response = await fetch(endPoint, init);
     const data: T = await response.json();
     if (!response.ok) {
-      console.error("[ERROR]", endPoint);
-      throw new Error("API 응답 처리 실패");
+      console.error('[ERROR]', endPoint);
+      throw new Error('API 응답 처리 실패');
     }
     return data;
   } catch (error) {
