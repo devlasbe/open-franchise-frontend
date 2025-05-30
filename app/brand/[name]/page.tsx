8012f8dcaba66efbfa09fd9abc3607808a714f04
lasbe <devlasbe@gmail.com>
2025-05-30 21:57:26 +0900

    feat: 업체 임시 비공개 처리

--- app/brand/[name]/page.tsx
+++ app/brand/[name]/page.tsx
@@ -1,12 +1,12 @@
-import { StatisticService } from "@/services/statistic";
-import BrandHeader from "../BrandHeader";
-import BrandStartup from "./BrandStartup";
-import BrandStatistic from "./BrandStatistic";
-import FetchBoundary from "@/components/errorBoundary/FetchBoundary";
-import BrandHead from "./BrandHead";
-import { BrandService } from "@/services/brand";
-import BrandInterior from "./BrandInterior";
-import { SeoUtil } from "@/utils/seo";
+import { StatisticService } from '@/services/statistic';
+import BrandHeader from '../BrandHeader';
+import BrandStartup from './BrandStartup';
+import BrandStatistic from './BrandStatistic';
+import FetchBoundary from '@/components/errorBoundary/FetchBoundary';
+import BrandHead from './BrandHead';
+import { BrandService } from '@/services/brand';
+import BrandInterior from './BrandInterior';
+import { SeoUtil } from '@/utils/seo';
 
 type BrandPageParams = {
   params: {
@@ -14,11 +14,16 @@ type BrandPageParams = {
   };
 };
 
+const filterList = ['%EB%9F%B0%EB%8D%98%EB%B2%A0%EC%9D%B4%EA%B8%80%EB%AE%A4%EC%A7%80%EC%97%84'];
+const getIsRejectedBrand = (name: string) => filterList.includes(name);
+
 export async function generateMetadata({ params: { name } }: BrandPageParams) {
   try {
     const brandResponse = await BrandService.getBrand(name);
     const { brand, head } = brandResponse?.payload;
-    const metadata = SeoUtil.metadata(`${brand?.brandNm} - ${head.jnghdqrtrsConmNm}`, true);
+    const metadata = getIsRejectedBrand(name)
+      ? SeoUtil.metadata()
+      : SeoUtil.metadata(`${brand?.brandNm} - ${head.jnghdqrtrsConmNm}`, true);
     return metadata;
   } catch (error) {
     console.error(error);
@@ -26,18 +31,47 @@ export async function generateMetadata({ params: { name } }: BrandPageParams) {
   }
 }
 
+const RejectedBrand = () => {
+  return (
+    <div className="flex-1 flex flex-col items-center justify-center">
+      <div className="flex flex-col gap-2">
+        <div className="text-sm text-gray-500">업체 측의 요청으로 인해 브랜드 정보가 비공개 처리되었습니다.</div>
+      </div>
+    </div>
+  );
+};
+
 export default function BrandPage({ params: { name } }: BrandPageParams) {
+  if (getIsRejectedBrand(name)) {
+    return (
+      <FetchBoundary>
+        <RejectedBrand />
+      </FetchBoundary>
+    );
+  }
+
   return (
     <FetchBoundary>
       <Layout name={name} />
     </FetchBoundary>
   );
 }
+
 const Layout = async ({ name }: { name: string }) => {
   const brandResponse = await BrandService.getBrand(name);
   const statisticDataList = (await StatisticService.getStatistic(name)).payload;
   const headData = brandResponse?.payload?.head;
   const brandData = statisticDataList?.length ? statisticDataList[0] : null;
+
+  const isRejectedBrand = filterList.includes(name);
+
+  if (isRejectedBrand) {
+    return (
+      <FetchBoundary>
+        <RejectedBrand />
+      </FetchBoundary>
+    );
+  }
 
   return (
     <div className="flex-1">
