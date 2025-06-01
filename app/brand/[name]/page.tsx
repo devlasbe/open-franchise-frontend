e28068765c5f16628e617257f55d239d75015538
lasbe <devlasbe@gmail.com>
2025-06-01 17:42:18 +0900

    feat: 브랜드 차단 기능 추가

--- app/brand/[name]/page.tsx
+++ app/brand/[name]/page.tsx
@@ -14,14 +14,12 @@ type BrandPageParams = {
   };
 };
 
-const filterList = ['%EB%9F%B0%EB%8D%98%EB%B2%A0%EC%9D%B4%EA%B8%80%EB%AE%A4%EC%A7%80%EC%97%84'];
-const getIsRejectedBrand = (name: string) => filterList.includes(name);
-
 export async function generateMetadata({ params: { name } }: BrandPageParams) {
   try {
     const brandResponse = await BrandService.getBrand(name);
+    const isRejectedBrand = brandResponse?.payload?.isRejectedBrand;
     const { brand, head } = brandResponse?.payload;
-    const metadata = getIsRejectedBrand(name)
+    const metadata = isRejectedBrand
       ? SeoUtil.metadata()
       : SeoUtil.metadata(`${brand?.brandNm} - ${head.jnghdqrtrsConmNm}`, true);
     return metadata;
@@ -42,14 +40,6 @@ const RejectedBrand = () => {
 };
 
 export default function BrandPage({ params: { name } }: BrandPageParams) {
-  if (getIsRejectedBrand(name)) {
-    return (
-      <FetchBoundary>
-        <RejectedBrand />
-      </FetchBoundary>
-    );
-  }
-
   return (
     <FetchBoundary>
       <Layout name={name} />
@@ -62,15 +52,10 @@ const Layout = async ({ name }: { name: string }) => {
   const statisticDataList = (await StatisticService.getStatistic(name)).payload;
   const headData = brandResponse?.payload?.head;
   const brandData = statisticDataList?.length ? statisticDataList[0] : null;
-
-  const isRejectedBrand = filterList.includes(name);
+  const isRejectedBrand = brandResponse?.payload?.isRejectedBrand;
 
   if (isRejectedBrand) {
-    return (
-      <FetchBoundary>
-        <RejectedBrand />
-      </FetchBoundary>
-    );
+    return <RejectedBrand />;
   }
 
   return (
